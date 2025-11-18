2cb61cccf9d991e1ff0722afcbecfc98c5e6c386
mysticalbeing21 <analiviaalmeidasantos02@gmail.com>
2025-11-18 08:29:14 -0300

    doc: Adicionado mais documentos e modificando variáveis

--- 11-11/access_fun.py
+++ 11-11/access_fun.py
@@ -1,33 +1,56 @@
-from users.jason import json as find_by_user
-from datetime import  datetime as d
+from db.access_file_user import *
+from datetime import  datetime as dt
 
 def login(user: str, password: str) -> bool:
-   
-    for k, v in db.items():
+   '''
+    Função para verificar se um login é válido.
+
+    Parâmetros: 
+        - user (str): nome do usuário
+        - password (str): senha do usuário
+    
+    Retorno: retornará um valor true ou false
+    '''
+   for k, v in df.items():
         if k == user and v == password:
             return True
-    
-    df = find_by_user(user)
-    if df.loc[0,2] == password:
-
-      return True
-    
-    return False
-
-
+        
+        df = find_by_user(user)
+   if df.iloc[0, 2] == password:
+        return True
+   
 def resetPassword(user: str,password: str, new_password: str) -> str:
+    '''
+    Função para redefinir a senha do usuário.
+
+    Parâmetros:
+        - user (str): nome do usuário
+        - password (str): senha do usuário
+        - new_password (str): senha atualizada para registrar
     
+    Retorno: Uma String confirmando ou recusando a modificação da senha
+    '''
     if login(user, password):
-        db[user] = new_password
+        df[user] = new_password
         return 'Senha Alterada!'
     
     return 'Usuário Não Cadastrado!'
 
 def update(user : str, password : str , new_user: str) -> str:
+    '''
+    Função para modificar o nome do usuário.
 
+    Parâmetros:
+        - user (str): nome do usuário
+        - password (str): senha do usuário
+        - new_user (str): novo nome do usuário
+    
+    Retorno: Confirmação da modificação do usuário. 
+            Caso seja disparado algum erro será gerado um log.
+    '''
     try:
         if login(user,password):
-            db[new_user] = db.pop(user)
+            df[new_user] = df.pop(user)
             return 'Usuário Atualizado'
     except:
         print(f'log:[{dt.date}] - [{dt.time}]: Error no servidor')
